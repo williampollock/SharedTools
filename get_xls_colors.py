ef34956415b2895780673b9ec4ae1733af7facd3
Oliver Hoopes <ohoopes@gmail.com>
2025-03-10 10:11:41 -0700

    Streamlined notebook

--- get_xls_colors.py
+++ get_xls_colors.py
@@ -1,15 +1,18 @@
-from colorsys import rgb_to_hls, hls_to_rgb
+from colorsys import hls_to_rgb, rgb_to_hls
+
+import matplotlib.pyplot as plt
 import openpyxl
 from openpyxl.utils import column_index_from_string
 
-#Here Oliver, I added something to your code!
+# Here Oliver, I added something to your code!
 
-#https://bitbucket.org/openpyxl/openpyxl/issues/987/add-utility-functions-for-colors-to-help
- 
-RGBMAX = 0xff  # Corresponds to 255
+# https://bitbucket.org/openpyxl/openpyxl/issues/987/add-utility-functions-for-colors-to-help
+
+RGBMAX = 0xFF  # Corresponds to 255
 HLSMAX = 240  # MS excel's tint function expects that HLS is base 240. see:
 # https://social.msdn.microsoft.com/Forums/en-US/e9d8c136-6d62-4098-9b1b-dac786149f43/excel-color-tint-algorithm-incorrect?forum=os_binaryfile#d3c2ac95-52e0-476b-86f1-e2a697f24969
- 
+
+
 def rgb_to_ms_hls(red, green=None, blue=None):
     """Converts rgb values in range (0,1) or a hex string of the form '[#aa]rrggbb' to HLSMAX based HLS, (alpha values are ignored)"""
     if green is None:
@@ -23,32 +26,49 @@ def rgb_to_ms_hls(red, green=None, blue=None):
             red, green, blue = red
     h, l, s = rgb_to_hls(red, green, blue)
     return (int(round(h * HLSMAX)), int(round(l * HLSMAX)), int(round(s * HLSMAX)))
- 
+
+
 def ms_hls_to_rgb(hue, lightness=None, saturation=None):
     """Converts HLSMAX based HLS values to rgb values in the range (0,1)"""
     if lightness is None:
         hue, lightness, saturation = hue
     return hls_to_rgb(hue / HLSMAX, lightness / HLSMAX, saturation / HLSMAX)
- 
+
+
 def rgb_to_hex(red, green=None, blue=None):
     """Converts (0,1) based RGB values to a hex string 'rrggbb'"""
     if green is None:
         red, green, blue = red
-    return ('%02x%02x%02x' % (int(round(red * RGBMAX)), int(round(green * RGBMAX)), int(round(blue * RGBMAX)))).upper()
- 
- 
+    return (
+        '%02x%02x%02x'
+        % (int(round(red * RGBMAX)), int(round(green * RGBMAX)), int(round(blue * RGBMAX)))
+    ).upper()
+
+
 def get_theme_colors(wb):
     """Gets theme colors from the workbook"""
     # see: https://groups.google.com/forum/#!topic/openpyxl-users/I0k3TfqNLrc
     from openpyxl.xml.functions import QName, fromstring
+
     xlmns = 'http://schemas.openxmlformats.org/drawingml/2006/main'
     root = fromstring(wb.loaded_theme)
     themeEl = root.find(QName(xlmns, 'themeElements').text)
     colorSchemes = themeEl.findall(QName(xlmns, 'clrScheme').text)
     firstColorScheme = colorSchemes[0]
- 
+
     colors = []
-    for c in ['lt1', 'dk1', 'lt2', 'dk2', 'accent1', 'accent2', 'accent3', 'accent4', 'accent5', 'accent6']:
+    for c in [
+        'lt1',
+        'dk1',
+        'lt2',
+        'dk2',
+        'accent1',
+        'accent2',
+        'accent3',
+        'accent4',
+        'accent5',
+        'accent6',
+    ]:
         accent = firstColorScheme.find(QName(xlmns, c).text)
         if accent is not None and len(accent) > 0:
             child = accent[0]
@@ -56,16 +76,9 @@ def get_theme_colors(wb):
                 colors.append(child.attrib['lastClr'])
             else:
                 colors.append(child.attrib['val'])
-    # for c in ['lt1', 'dk1', 'lt2', 'dk2', 'accent1', 'accent2', 'accent3', 'accent4', 'accent5', 'accent6']:
-    #     accent = firstColorScheme.find(QName(xlmns, c).text)
- 
-    #     if 'window' in accent.getchildren()[0].attrib['val']:
-    #         colors.append(accent.getchildren()[0].attrib['lastClr'])
-    #     else:
-    #         colors.append(accent.getchildren()[0].attrib['val'])
- 
     return colors
- 
+
+
 def tint_luminance(tint, lum):
     """Tints a HLSMAX based luminance"""
     # See: http://ciintelligence.blogspot.co.uk/2012/02/converting-excel-theme-color-and-tint.html
@@ -73,26 +86,14 @@ def tint_luminance(tint, lum):
         return int(round(lum * (1.0 + tint)))
     else:
         return int(round(lum * (1.0 - tint) + (HLSMAX - HLSMAX * (1.0 - tint))))
- 
+
+
 def theme_and_tint_to_rgb(wb, theme, tint):
     """Given a workbook, a theme number and a tint return a hex based rgb"""
     rgb = get_theme_colors(wb)[theme]
     h, l, s = rgb_to_ms_hls(rgb)
     return rgb_to_hex(ms_hls_to_rgb(h, tint_luminance(tint, l), s))
 
-# ##### EXAMPLE USAGE ######
-
-# # Load the Excel file
-# workbook = openpyxl.load_workbook(path_to_xlsx)
-# theme = cell.fill.start_color.theme
-# tint = cell.fill.start_color.tint
-                                        
-# #Get a cell (D2):
-# cell = sheet['D2']
-
-# #get the color of the cell:
-# color = theme_and_tint_to_rgb(workbook, theme, tint)
-# color
 
 def convert_hex(hex_code):
     """
@@ -113,13 +114,11 @@ def convert_hex(hex_code):
 
     return hex_code
 
-# Example usage:
-# matplotlib_compatible_hex = convert_hex('FFDFF4FD')
-# This will output '#DFF4FDFF'
 
 def get_cell_colors(workbook, sheet_name, min_row, min_col, max_row=None, max_col=None):
-    '''Extracts cell shading colors from a range of cells in an Excel worksheet.
-    
+    """
+    Extracts cell shading colors from a range of cells in an Excel worksheet.
+
     Parameters
     ----------
     workbook : openpyxl.Workbook
@@ -132,15 +131,15 @@ def get_cell_colors(workbook, sheet_name, min_row, min_col, max_row=None, max_co
         The first column to extract colors from.
     max_row : int, optional
         The last row to extract colors from. If None, the last row in the worksheet is used.
-    max_col : int, optional 
+    max_col : int, optional
         The last column to extract colors from. If None, the last column in the worksheet is used.
 
     Returns
     -------
     dict
         A dictionary with keys as (row, column) tuples and values as the color of the cell.
-    
-    '''
+
+    """
     # Get the worksheet
     sheet = workbook[sheet_name]
 
@@ -149,36 +148,29 @@ def get_cell_colors(workbook, sheet_name, min_row, min_col, max_row=None, max_co
         max_row = sheet.max_row
     if max_col is None:
         max_col = sheet.max_column
-    
+
     # Create an empty dictionary to store the colors
     c_dict = {}
     colors = []
     for row in sheet.iter_rows(min_row=min_row, max_row=max_row, min_col=min_col, max_col=max_col):
         for cell in row:
             color_idx = cell.fill.start_color.index
-            
+
             if isinstance(color_idx, str):
                 color = convert_hex(color_idx)
             elif isinstance(color_idx, int):
                 theme = cell.fill.start_color.theme
                 tint = cell.fill.start_color.tint
-                color = theme_and_tint_to_rgb(workbook,theme, tint)
+                color = theme_and_tint_to_rgb(workbook, theme, tint)
                 color = convert_hex(color)
             else:
                 color = 'white'
-            #get column letter:
+            # get column letter:
             col_letter = openpyxl.utils.get_column_letter(cell.column)
             # print(f'Cell {col_letter}{cell.row}: {color}')
             c_dict[(col_letter, cell.row)] = color
             colors.append(color)
     return colors, c_dict
-
-# ##### EXAMPLE USAGE ######
-
-# # Load the Excel file
-# workbook = openpyxl.load_workbook(path_to_xlsx)
-# #get the colors of the cells in column D starting at row 2 and ending at the last row:
-# colors, c_dict = get_cell_colors(workbook, name_of_sheet, 2, 4, max_col=4)
 
 
 def get_value_color_map(workbook, sheet_name, min_row, min_col, max_row=None, max_col=None):
@@ -198,7 +190,7 @@ def get_value_color_map(workbook, sheet_name, min_row, min_col, max_row=None, ma
         The first column to extract colors from.
     max_row : int, optional
         The last row to extract colors from. If None, the last row in the worksheet is used.
-    max_col : int, optional 
+    max_col : int, optional
         The last column to extract colors from. If None, the last column in the worksheet is used.
 
     Returns
@@ -210,17 +202,60 @@ def get_value_color_map(workbook, sheet_name, min_row, min_col, max_row=None, ma
 
     # Reuse the existing function to get the list of colors and dict of (column_letter, row) -> color
     colors, c_dict = get_cell_colors(workbook, sheet_name, min_row, min_col, max_row, max_col)
-    
+
     # Retrieve the sheet so we can look up the actual cell values
     sheet = workbook[sheet_name]
-    
+
     # Build a dictionary that maps cell values to color hex codes
     value_color_map = {}
     for (col_letter, row), color_hex in c_dict.items():
         col_index = column_index_from_string(col_letter)
         cell_value = sheet.cell(row=row, column=col_index).value
-        
+
         # Store/overwrite the color keyed by the cell's value
         value_color_map[cell_value] = color_hex
 
     return value_color_map
+
+
+def plot_colored_boxes(value_color_map, cols=4, box_size=(2, 1), figname=None):
+    """
+    Creates a visualization of the value_color_map dictionary.
+
+    Parameters
+    ----------
+    value_color_map
+
+    """
+
+    keys = list(value_color_map.keys())
+    rows = (len(keys) + cols - 1) // cols  # Compute required rows
+    fig, ax = plt.subplots(figsize=(cols * box_size[0], rows * box_size[1]))
+    ax.set_xlim(0, cols)
+    ax.set_ylim(0, rows)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_frame_on(False)
+
+    for idx, key in enumerate(keys):
+        row = rows - 1 - (idx // cols)  # Reverse row order for plotting
+        col = idx % cols
+        color = value_color_map[key]
+
+        rect = plt.Rectangle((col, row), 1, 1, color=color, ec='black')
+        ax.add_patch(rect)
+        ax.text(
+            col + 0.5,
+            row + 0.5,
+            key,
+            ha='center',
+            va='center',
+            fontsize=12,
+            weight='bold',
+            color='white' if int(color[1:], 16) < 0x888888 else 'black',
+        )
+
+    if figname:
+        fig.savefig(figname)
+
+    plt.show()
